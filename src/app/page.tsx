6d94511d416482f6fb910486178dc2fb248eefa3
Alan silva <allansillva313@gmail.com>
2023-11-20 14:45:00 -0300

    Estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import Product from "./components/Product";
+import React from 'react'
 import { ProductType } from '@/types/ProductType'
+
+
 
 async function getProducts(){
   const res = await fetch('https://fakestoreapi.com/products')
@@ -14,19 +18,24 @@ async function getProducts(){
 export default async function Home() {
   const products = await getProducts();
 
-  console.log(products)
+
+
+
   return (
     <>
 
-    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+<div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        {products.map((product: ProductType) => (
-           <div key={product.id}>{product.title}</div>
-        ))}
-      </div>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+  {products.map((product: ProductType) => (
+     <Product key={product.id} product={product}>
+
    
-    </div>
+      </Product>
+  ))}
+</div>
+
+</div>
 
     
     </>
